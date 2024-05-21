891a5576f8715f061cf68cdc734518cfab6f7c8b
jammie-commits <jay.mbugua.ph@gmail.com>
2024-05-21 13:40:22 +0300

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")    
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+    
 
-def add(num1, num2):
-    pass
+def add(num1 = "45", num2 = "55"):
+    return int(num1) + int(num2)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)): 
+       return None
+
+    return number / 2
